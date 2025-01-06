c5812845a7b45560f4e03350bb9344176981d86a
ahan10 <ahanbhargava10@gmail.com>
2025-01-05 19:42:45 -0500

    [ADD] Stack solutions with explanation

--- Stacks/EvaluateReversePolishNotation.py
+++ Stacks/EvaluateReversePolishNotation.py
@@ -1,5 +1,6 @@
 # 150. Evaluate Reverse Polish Notation
 # https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+from math import ceil, floor
 from typing import List
 
 
@@ -8,26 +9,20 @@ class Solution:
         stk = []
 
         for i in tokens:
-            if i == '+': # if there is +, pop the last two, add then and put it in the stack
-                b = stk.pop()
-                a = stk.pop()
-
-                stk.append(a + b)
-            elif i == '*': # if there is *, pop the last two, multiply then and put it in the stack
-                b = stk.pop()
-                a = stk.pop()
-
-                stk.append(a * b)
-            elif i == '/': # if there is /, pop the last two, divide then and put it in the stack
-                b = stk.pop()
-                a = stk.pop()
-
-                stk.append(int(a / b)) # int(a/b) truncates the answer towards 0
-            elif i == '-': # if there is -, pop the last two, subtract b from a then and put it in the stack
-                b = stk.pop()
-                a = stk.pop()
-
-                stk.append(a - b)
+            if i in '+-*/': # if there is any operation, pop the last two
+                b, a = stk.pop(), stk.pop()
+                if i == '+': # if there is +, add then and put it in the stack
+                    stk.append(a + b)
+                elif i == '*': # if there is *, multiply then and put it in the stack
+                    stk.append(a * b)
+                elif i == '-':  # if there is -, subtract b from a then and put it in the stack
+                    stk.append(a - b)
+                else: # if there is /, divide then and put it in the stack
+                    div = a/b
+                    if div < 0: # if the division result is negative store the ceiling value
+                        stk.append(ceil(div))
+                    else: # if the division result is positive store the floor value
+                        stk.append(floor(div))
             else: # else put the integer version in
                 stk.append(int(i))
 
