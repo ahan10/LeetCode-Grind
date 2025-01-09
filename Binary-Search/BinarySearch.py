a2613ebd5f7f6b37999e3b8b02e163a27a1718a8
ahan10 <ahanbhargava10@gmail.com>
2025-01-08 22:45:34 -0500

    [ADD] Binary Search solution with explanation

---
+++ Binary-Search/BinarySearch.py
@@ -0,0 +1,20 @@
+# 704. Binary Search
+# https://leetcode.com/problems/binary-search/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1 # 2 pointers at left and right
+
+        while left <= right: # until the pointers aren't equal
+            middle = (left + right) // 2 # we find the middle index
+            if nums[middle] == target: # if the nums at middle index is the target, return the index, and job is done
+                return middle
+            elif nums[middle] > target: # if the nums at middle index is larger than the target, then the number potentially lies between left pointer and middle, so now update the right pointer to just before the middle index
+                right = middle - 1
+            else: # if the nums at middle index is smaller than the target, then the number potentially lies between middle pointer and right pointer, so now update the left pointer to just after the middle index
+                left = middle + 1
+        # if the element is not found return -1
+        return -1
+
+# Time: O(n log n)
+# Space: O(1)
